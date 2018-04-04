b859e13e20e6ec3817989ec3adaabdb993705415
Prasad Acharya <ps274@hw.ac.uk>
2018-04-04 22:01:59 +0400

    Modified Project

--- server.js
+++ server.js
@@ -1,22 +1,60 @@
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
+//
+// const hostname = '127.0.0.1';
+// const port = 3000;
+//
+// fs.readFile('src/index.html', (err, html) => {
+//   if(err){
+//     throw err;
+//   }
+//
+//   const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-type', 'text/html');
+//     res.write(html);
+//     res.end();
+//   });
+//
+//   server.listen(port, hostname, () => {
+//     console.log('Server started on port ' + port);
+//   });
+// });
 
-const hostname = '127.0.0.1';
-const port = 3000;
+let express = require("express");
+let bodyParser = require("body-parser");
+let path = require("path");
+let pg = require("pg");
+let flash = require('connect-flash');
 
-fs.readFile('src/index.html', (err, html) => {
-  if(err){
-    throw err;
-  }
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-  const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/html');
-    res.write(html);
-    res.end();
-  });
+// Chat application components
+var routes 		= require('./src/utils/routes.js');
+var session 	= require('./src/utils/session.js');
+var passport    = require('./src/utils/auth.js');
+var io 	= require('./src/utils/socket.js')(app);
 
-  server.listen(port, hostname, () => {
-    console.log('Server started on port ' + port);
-  });
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.use(session);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use('/', routes);
+
+// Middleware to catch 404 errors
+app.use(function(req, res, next) {
+  res.status(404).sendFile(process.cwd() + '/app/views/404.htm');
+});
+
+io.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
 });
---
+++ src/utils/session.js
@@ -0,0 +1,30 @@
+let session 	= require('express-session');
+let MongoStore	= require('connect-mongo')(session);
+let db 		    = require('../db');
+let config 		= require('../config');
+
+/**
+ * Initialize Session
+ * Uses MongoDB-based session store
+ *
+ */
+var init = function () {
+	if(process.env.NODE_ENV === 'production') {
+		return session({
+			secret: config.sessionSecret,
+			resave: false,
+			saveUninitialized: false,
+			unset: 'destroy',
+			store: new MongoStore({ mongooseConnection: db.Mongoose.connection })
+		});
+	} else {
+		return session({
+			secret: config.sessionSecret,
+			resave: false,
+			unset: 'destroy',
+			saveUninitialized: true
+		});
+	}
+}
+
+module.exports = init();
